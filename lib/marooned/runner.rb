81e7509197e6f04d8fe989ce408a10e1e3cc9d03
Keith Smiley <keithbsmiley@gmail.com>
2014-08-01 11:57:57 -0700

    Test initialization with/without project

--- lib/marooned/runner.rb
+++ lib/marooned/runner.rb
@@ -1,7 +1,8 @@
 module Marooned
   class Runner
     def run(options)
-      options = set_default_project(options)
+      set_default_project(options)
+      options
     end
 
     private
@@ -12,7 +13,7 @@ module Marooned
 
     def first_xcode_project
       projects = Dir.glob("*.xcodeproj")
-      projects = projects.reject(&:is_pods_project)
+      projects = projects.reject(&method(:is_pods_project))
       exit_project_files unless projects.length > 0
       projects.first
     end
@@ -22,7 +23,7 @@ module Marooned
     end
 
     def exit_project_files
-      puts "No .xcodeproject files available"
+      STDERR.puts "No xcodeproj files available"
       exit 1
     end
   end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -2,5 +2,20 @@ require "spec_helper"
 
 describe Marooned::Runner do
   describe "run" do
+    it "should set a default project" do
+      allow(Dir).to receive(:glob).and_return(["foo.xcodeproj"])
+      options = Marooned::Runner.new.run({})
+      expect(options[:project]).to eq("foo.xcodeproj")
+    end
+
+    it "should fail if there are no projects" do
+      allow(Dir).to receive(:glob).and_return([])
+      expect { Marooned::Runner.new.run({}) }.to raise_exception SystemExit
+    end
+
+    it "should not overwrite the passed project" do
+      options = Marooned::Runner.new.run({project: "foo.xcodeproj"})
+      expect(options[:project]).to eq("foo.xcodeproj")
+    end
   end
 end
