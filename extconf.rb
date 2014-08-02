426d68ed0ee99861c188512d46ea6677fbcb2231
Keith Smiley <keithbsmiley@gmail.com>
2014-08-01 19:31:49 -0700

    Update gemspec

---
+++ extconf.rb
@@ -0,0 +1,11 @@
+$stderr.puts
+$stderr.puts
+$stderr.puts "************************************************************"
+$stderr.puts
+$stderr.puts "Hey, you! marooned is not installable as a gem!"
+$stderr.puts
+$stderr.puts "See the README at: https://github.com/Keithbsmiley/marooned"
+$stderr.puts
+$stderr.puts "************************************************************"
+$stderr.puts
+$stderr.puts
